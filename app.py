50e736bec2e2b94da54691a3f14bb410ef623112
Binh An <anbinhpham110799@gmail.com>
2022-10-15 18:23:29 +0700

    taking calendars offline to fix memory leaks

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Tri Viet Education",
+    page_icon=":pager:",
+    initial_sidebar_state="expanded",
+)
+
+st.title('Internal Tools for TVE')
