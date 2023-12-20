dbb7b93ab9df8fc425a1f583fcd2fd4ba5060a61
AnP <53161441+0xanp@users.noreply.github.com>
2023-12-20 17:47:30 +0700

    Update Add_Course_New.py

--- pages/Add_Course_New.py
+++ pages/Add_Course_New.py
@@ -33,7 +33,7 @@ def load_options():
     options.add_argument("--no-sandbox")
     driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))#version="114.0.5735.16").install()))
     # login page
-    driver.get("https://trivietedu.ileader.vn/login.aspx")
+    driver.get("http://trivietedu.ileader.vn/login.aspx")
     # find username/email field and send the username itself to the input field
     driver.find_element("id","user").send_keys(MANAGER_USERNAME)
     # find password input field and insert password as well
@@ -41,7 +41,7 @@ def load_options():
     # click login button
     driver.find_element(By.XPATH,'//*[@id="login"]/button').click()
 
-    driver.get('https://trivietedu.ileader.vn/Default.aspx?mod=lophoc!lophoc_baihoc')
+    driver.get('http://trivietedu.ileader.vn/Default.aspx?mod=lophoc!lophoc_baihoc')
 
     class_select = Select(WebDriverWait(driver, 2).until(
                 EC.element_to_be_clickable((By.XPATH,'//*[@id="idlophoc"]'))))
